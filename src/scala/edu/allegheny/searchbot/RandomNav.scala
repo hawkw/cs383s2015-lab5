a67eb1928c7edf8a8d7a715c3626cabdfb30b949
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-04 10:54:14 -0500

    Almost finishes random walk

--- src/scala/edu/allegheny/searchbot/RandomNav.scala
+++ src/scala/edu/allegheny/searchbot/RandomNav.scala
@@ -5,7 +5,10 @@ import scala.util.Random
 object RandomNav {
 
     val maxDist = 50 // TODO: placeholder, replace this with correct value
-    val headings:   Stream[Int] = Random.nextInt(360)       :: headings
-    val distances:  Stream[Int] = Random.nextInt(maxDist)   :: distances
+    val headings:   Stream[Int]         = Random nextInt(360)       :: headings
+    val distances:  Stream[Int]         = Random nextInt(maxDist)   :: distances
+    val moves:      Stream[(Int,Int)]   = headings zip distances
+
+    def main(argv: Array[String]) = moves.foreach{ (heading, dist) => ??? }
 
 }
