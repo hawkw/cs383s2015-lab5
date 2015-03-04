f1eb13b4b4461896f6916abf42fa0748fb3a4ed8
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-04 11:57:41 -0500

    Should now break on button press

--- src/scala/edu/allegheny/searchbot/RandomNav.scala
+++ src/scala/edu/allegheny/searchbot/RandomNav.scala
@@ -1,17 +1,22 @@
 package edu.allegheny.searchbot;
 
 import scala.util.Random.nextInt
+import lejos.hardware.BrickFinder
+import lejos.hardware.ev3.EV3
 
-object RandomNav {
+object RandomNav extends App {
 
-    val maxDist = 50 // TODO: placeholder, replace this with correct value
-    val moves   = Stream continually ((nextInt(360), nextInt(maxDist)))
+    val maxDist = 50 // TODO: placeholder, replace this with correct values
+    val keys    = BrickFinder.getLocal.asInstanceOf[EV3].getKeys
+    val moves   = Stream continually ((nextInt(360), nextInt(maxDist), keys readButtons))
     val pilot   = searchbot getPilot
 
-    def main(argv: Array[String]) = moves foreach {
-        // TODO: break on button press
+    def main(argv: Array[String]) = moves
+    takeWhile {
+        (_,_,buttons) => buttons == 0
+    } foreach {
         // TODO: possibly some kind of collision avoidance?
-        (heading, dist) =>
+        (heading, dist, _) =>
             pilot rotate heading
             pilot travel dist
     }
