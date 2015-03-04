fd14a2519525d331abaf7277f3801ac5335c9922
Hawk Weisman <hawk.weisman@gmail.com>
2015-03-04 10:32:26 -0500

    lol tabs

--- src/scala/edu/allegheny/searchbot/RandomNav.scala
+++ src/scala/edu/allegheny/searchbot/RandomNav.scala
@@ -5,7 +5,7 @@ import scala.util.Random
 object RandomNav {
 
     val maxDist = 50 // TODO: placeholder, replace this with correct value
-    val headings: Stream[Int]   = Random.nextInt(360)     :: headings
-    val distances: Stream[Int]  = Random.nextInt(maxDist) :: distances
+    val headings:   Stream[Int] = Random.nextInt(360)       :: headings
+    val distances:  Stream[Int] = Random.nextInt(maxDist)   :: distances
 
 }
