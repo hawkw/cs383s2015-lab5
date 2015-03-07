67b4639c9f8072d8281c1d15e835c2be5912affc
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-07 11:20:21 -0500

    QUick first pass on ranging

--- src/scala/edu/allegheny/searchbot/Search.scala
+++ src/scala/edu/allegheny/searchbot/Search.scala
@@ -1,5 +1,24 @@
 package edu.allegheny.searchbot;
 
 object Search {
-    // TODO: implement me pls
+
+    val rangeProvider = searchbot getDistanceMode
+    val MaxRange = 50.0f // change as appropriate
+    private val sample = new Array[Float](1) // this is because the LeJOS api is awful
+
+    /**
+     * Gets a range from the ultrasonic sensor.
+     * Returns a Some(Float) if something is within range,
+     * None if no objects were detected within the search range.
+     * @type {Option[Float]}
+     */
+    def range: Option[Float] = {
+        rangeProvider fetchSample(sample)
+        sample match {
+            case Array(i if i <= MaxRange)  => Some(i)
+            case _                          => None
+        }
+    }
+
+
 }
