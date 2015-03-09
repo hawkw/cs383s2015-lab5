ed43fce56a2107f0460a1618bd4caf0c3afe36f5
Hawk Weisman <hawk@meteorcodelabs.com>
2015-03-09 10:41:20 -0400

    Search.range() makes tuples with angle now

--- src/scala/edu/allegheny/searchbot/Search.scala
+++ src/scala/edu/allegheny/searchbot/Search.scala
@@ -1,22 +1,28 @@
 package edu.allegheny.searchbot;
 
+/**
+ * The search program.
+ * @author Hawk Weisman
+ */
 object Search {
-
+    val angleProvider = searchbot getAngleMode
     val rangeProvider = searchbot getDistanceMode
     val MaxRange = 50.0f // change as appropriate
-    private val sample = new Array[Float](1) // this is because the LeJOS api is awful
+    private val sample = new Array[Float](2) // this is because the LeJOS api is awful
 
     /**
-     * Gets a range from the ultrasonic sensor.
-     * Returns a Some(Float) if something is within range,
+     * Gets a range from the ultrasonic sensor and the current heading
+     * from the gyro.
+     * Returns a Some((Float, Float) if something is within range,
      * None if no objects were detected within the search range.
-     * @type {Option[Float]}
+     * @type {Option[(Float,Float)]}
      */
-    def range: Option[Float] = {
-        rangeProvider fetchSample(sample)
+    def range: Option[(Float,Float)] = {
+        rangeProvider fetchSample (sample)
+        angleProvider fetchSample (sample,1)
         sample match {
-            case Array(i if i <= MaxRange)  => Some(i)
-            case _                          => None
+            case Array(r if r <= MaxRange, a)   => Some((i,a))
+            case _                              => None
         }
     }
 
